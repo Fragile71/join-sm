e36f4bd116d49675c1c6fcab6c81f15fe4370d76
user <user@DESKTOP-ERR5D88>
2023-10-24 16:52:33 +0900

    sm-10-24

---
+++ JustShare/src/main/java/com/js/web/join/JoinDAO.java
@@ -0,0 +1,12 @@
+package com.js.web.join;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface JoinDAO{
+	public int join(JoinDTO joinDTO);
+	
+	
+}
+
+
---
+++ JustShare/src/main/java/com/js/web/join/JoinDTO.java
@@ -0,0 +1,11 @@
+package com.js.web.join;
+
+import lombok.Data;
+
+@Data
+public class JoinDTO {
+
+	private int mno, mgrade;
+	private String mid, mpw,mname,addr, mbirth,mphone,memail,mjoindate ;
+	
+}
